3212acc9452bd61a390ef1384f2a3c8c4ee31978
Harrison Chen <harryandc@gmail.com>
2026-05-04 16:54:03 -0700

    test: cover terminal key mappings

--- Tests/WandaTests/TerminalKeyMapperTests.swift
+++ Tests/WandaTests/TerminalKeyMapperTests.swift
@@ -26,4 +26,27 @@ final class TerminalKeyMapperTests: XCTestCase {
         XCTAssertEqual(mapper.bytes(for: .special(.leftArrow, modifiers: [.command])), [0x01])
         XCTAssertEqual(mapper.bytes(for: .special(.rightArrow, modifiers: [.command])), [0x05])
     }
+
+    func testPlainArrowsMapToEscapeSequences() {
+        let mapper = TerminalKeyMapper()
+
+        XCTAssertEqual(mapper.bytes(for: .special(.leftArrow, modifiers: [])), Array("\u{001B}[D".utf8))
+        XCTAssertEqual(mapper.bytes(for: .special(.rightArrow, modifiers: [])), Array("\u{001B}[C".utf8))
+        XCTAssertEqual(mapper.bytes(for: .special(.upArrow, modifiers: [])), Array("\u{001B}[A".utf8))
+        XCTAssertEqual(mapper.bytes(for: .special(.downArrow, modifiers: [])), Array("\u{001B}[B".utf8))
+    }
+
+    func testReturnDeleteAndTabMapToControlBytes() {
+        let mapper = TerminalKeyMapper()
+
+        XCTAssertEqual(mapper.bytes(for: .special(.returnKey, modifiers: [])), [0x0D])
+        XCTAssertEqual(mapper.bytes(for: .special(.delete, modifiers: [])), [0x7F])
+        XCTAssertEqual(mapper.bytes(for: .special(.tab, modifiers: [])), [0x09])
+    }
+
+    func testOptionTakesPrecedenceOverCommandForCombinedLeftArrowModifier() {
+        let mapper = TerminalKeyMapper()
+
+        XCTAssertEqual(mapper.bytes(for: .special(.leftArrow, modifiers: [.option, .command])), [0x1B, UInt8(ascii: "b")])
+    }
 }
