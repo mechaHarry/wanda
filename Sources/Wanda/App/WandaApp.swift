41baa6864540943a995839043a1333ccef693176
Harrison Chen <harryandc@gmail.com>
2026-05-05 14:51:48 -0700

    fix: foreground activate app launch

--- Sources/Wanda/App/WandaApp.swift
+++ Sources/Wanda/App/WandaApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct WandaApp: App {
+    @NSApplicationDelegateAdaptor(WandaApplicationDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup("Wanda") {
             TerminalWindowView()
